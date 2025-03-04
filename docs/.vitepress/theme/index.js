7db7bc0637efe95e672ebfcdbc73e6a66cc9ff47
haowei lu <53548271+shen2255678@users.noreply.github.com>
2025-03-04 21:07:01 +0800

    簡化主題設置，移除Layout.vue引用

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,13 +4,11 @@ import './tailwind.css'
 
 // 引入自定義組件
 import NotFound from './NotFound.vue'
-import Layout from './Layout.vue'
 
 export default {
   ...DefaultTheme,
   // 覆蓋 NotFound 組件
   NotFound,
-  Layout,
   enhanceApp({ app }) {
     // 註冊其他組件（如果需要）
   }
