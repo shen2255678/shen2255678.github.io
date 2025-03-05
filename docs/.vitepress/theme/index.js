1cf66c11bbe737d4f686fea754df892bbfb323e5
haowei lu <53548271+shen2255678@users.noreply.github.com>
2025-03-05 21:20:29 +0800

    更新 VitePress 主題配置，全局註冊 vue-easy-lightbox

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,11 +5,17 @@ import './tailwind.css'
 // 引入自定義組件
 import NotFound from './NotFound.vue'
 
+// 引入 vue-easy-lightbox
+import VueEasyLightbox from 'vue-easy-lightbox'
+
 export default {
   ...DefaultTheme,
   // 覆蓋 NotFound 組件
   NotFound,
   enhanceApp({ app }) {
+    // 註冊 vue-easy-lightbox
+    app.use(VueEasyLightbox)
+    
     // 註冊其他組件（如果需要）
   }
 }
